32ff899708b57935d12fa1b10c2082c11e01a20d
Elmar Hinz <t3elmar@gmail.com>
2015-12-27 01:51:57 +0100

    Adding window

--- vii/view/commandline.py
+++ vii/view/commandline.py
@@ -1,16 +1,9 @@
-import curses
+from .abstractwindow import AbstractWindow
 
-class CommandLine:
+class CommandLine(AbstractWindow):
 
-    def __init__(self, parentWindow):
+    def layout(self, parentWindow):
         height, width = 1, parentWindow.getmaxyx()[1]
         x, y = 0, parentWindow.getmaxyx()[0] - 1
-        window = curses.newwin(height, width, y, x)
-        window.refresh()
-        self.window = window
-
-    def draw(self, text):
-        self.window.clear()
-        self.window.addstr(text)
-        self.window.refresh()
+        return (height, width, y, x)
 
