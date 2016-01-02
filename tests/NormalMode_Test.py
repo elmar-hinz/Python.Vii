ee942bef837064c03b3460e8b2cdd68f893f82fd
Elmar Hinz <t3elmar@gmail.com>
2016-01-02 14:40:18 +0100

    Fix tests to follow restructuring

--- tests/NormalMode_Test.py
+++ tests/NormalMode_Test.py
@@ -19,20 +19,23 @@ class Mock:
     def draw(self):
         print("draw called")
 
+    def insertLines(self, y, l):
+        print("insertLines called")
+
 class TestNormalMode:
 
     def setup(self):
-        cursor, buffer, window, view, model = (
-                Mock(), Mock(), Mock(), Mock(), Mock())
-        buffer.cursor, window.buffer, view.window, model.buffer = (
-                cursor, buffer, window, buffer)
-        self.fixture = NormalMode(model, view)
+        cursor, buffer, window, view = (
+                Mock(), Mock(), Mock(), Mock())
+        buffer.cursor, window.buffer, view.window = (
+                cursor, buffer, window)
+        self.fixture = NormalMode(view, buffer)
 
     def teardown(self):
         pass
 
     def testInit(self):
-        for member in (" model view window buffer cursor "
+        for member in (" view window buffer cursor "
         " command commandMap delegating " " insertMode ").split():
             assert member in dir(self.fixture)
 
