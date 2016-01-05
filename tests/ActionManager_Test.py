88c97f021eb305f5000954ed9f65f7d1f3e1182a
Elmar Hinz <t3elmar@gmail.com>
2016-01-05 13:26:28 +0100

    Creating and testing ActionManger

---
+++ tests/ActionManager_Test.py
@@ -0,0 +1,49 @@
+import sys
+from vii.ActionManager import ActionManager
+from vii.AbstractAction import AbstractAction
+
+class Alpha:
+    def __init__(self, dispatcher, windowManager):
+        self.dispatcher = dispatcher
+        self.windowManager = windowManager
+
+class ActionManager_Test:
+
+    mode = "mmm"
+    operator = "a"
+    actionMap = {"a":"alpha", "b":"beta"}
+    mapString = """
+    a: alpha
+    b: beta
+    """
+
+    def setup(self):
+        self.dispatcher = "Dispatcher"
+        self.windowManager = "WM"
+        self.manager = ActionManager(self.dispatcher, self.windowManager)
+
+    def test_parseMap(self):
+        result = self.manager.parseMap(self.mapString)
+        assert result == self.actionMap
+
+    def test_addMap(self):
+        self.manager.addMap(self.mode, self.mapString)
+        result = self.manager.actionMaps[self.mode]
+        assert result == self.actionMap
+
+    def test_addModule(self):
+        me = sys.modules[__name__]
+        self.manager.addModule(self.mode, me)
+        result = self.manager.actionModules[self.mode]
+        assert result == me
+
+    def test_action(self):
+        me = sys.modules[__name__]
+        self.manager.addModule(self.mode, me)
+        self.manager.addMap(self.mode, self.mapString)
+        action = self.manager.action("mmm", "a")
+        assert action.__class__ == Alpha
+        assert action.windowManager == self.windowManager
+        assert action.dispatcher == self.dispatcher
+
+
