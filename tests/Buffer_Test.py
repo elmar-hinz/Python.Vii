451f373912922ad220e0b956deb7a11adc997fd3
Elmar Hinz <t3elmar@gmail.com>
2016-01-18 13:26:04 +0100

    Makeing buffer based1

--- tests/Buffer_Test.py
+++ tests/Buffer_Test.py
@@ -44,7 +44,7 @@ class TestBuffer:
     def test_lengthOfLine(self):
         text = "1\n22\n333"
         self.buffer.fill(text)
-        assert self.buffer.lengthOfLine(2) == 3
+        assert self.buffer.lengthOfLine(3) == 3
 
     def test_fill(self):
         text = "1\n2\n3"
@@ -63,104 +63,104 @@ class TestBuffer:
     def test__checkBufferBounds_validInnerRange(self):
         text = "1\n2\n3"
         self.buffer.fill(text)
-        self.buffer._checkBufferBounds(0)
-        self.buffer._checkBufferBounds(2)
+        self.buffer._checkBufferBounds(1)
+        self.buffer._checkBufferBounds(3)
         assert True
 
     @raises(BufferBoundsException)
     def test__checkBufferBounds_negativeException(self):
         text = "1\n2\n3"
         self.buffer.fill(text)
-        self.buffer._checkBufferBounds(-1)
+        self.buffer._checkBufferBounds(0)
 
     @raises(BufferBoundsException)
     def test__checkBufferBounds_positiveException(self):
         text = "1\n2\n3"
         self.buffer.fill(text)
-        self.buffer._checkBufferBounds(3)
+        self.buffer._checkBufferBounds(4)
 
     def test__checkBufferBoundsPlus1_validInnerRange(self):
         text = "1\n2\n3"
         self.buffer.fill(text)
-        self.buffer._checkBufferBoundsPlus1(0)
-        self.buffer._checkBufferBoundsPlus1(3)
+        self.buffer._checkBufferBoundsPlus1(1)
+        self.buffer._checkBufferBoundsPlus1(4)
         assert True
 
     @raises(BufferBoundsException)
     def test__checkBufferBoundsPlus1_negativeException(self):
         text = "1\n2\n3"
         self.buffer.fill(text)
-        self.buffer._checkBufferBoundsPlus1(-1)
+        self.buffer._checkBufferBoundsPlus1(0)
 
     @raises(BufferBoundsException)
     def test__checkBufferBoundsPlus1_positiveException(self):
         text = "1\n2\n3"
         self.buffer.fill(text)
-        self.buffer._checkBufferBoundsPlus1(4)
+        self.buffer._checkBufferBoundsPlus1(5)
 
     """ copying """
 
     def test_copyLines(self):
         text = "1\n2\n3\n4"
         self.buffer.fill(text)
-        assert self.buffer.copyLines(1, 2) == "2\n3"
+        assert self.buffer.copyLines(2, 2) == "2\n3"
 
     def test_copyAllLines(self):
         text = "1\n2\n3\n4"
         self.buffer.fill(text)
-        assert self.buffer.copyLines(0, 4) == text
+        assert self.buffer.copyLines(1, 4) == text
 
     def test_copyNullLines(self):
         text = "1\n2\n3\n4"
         self.buffer.fill(text)
-        assert self.buffer.copyLines(0, 0) == ""
+        assert self.buffer.copyLines(1, 0) == ""
 
     def test_copyFromLine(self):
         text = "1\n1234"
         self.buffer.fill(text)
-        assert self.buffer.copyFromLine((1,1),2) == "23"
+        assert self.buffer.copyFromLine((2,2),2) == "23"
 
     def test_copyAllFromLine(self):
         text = "1\n1234"
         self.buffer.fill(text)
-        assert self.buffer.copyFromLine((1,0),4) == "1234"
+        assert self.buffer.copyFromLine((2,1),4) == "1234"
 
     def test_copyNullFromLine(self):
         text = "1\n123"
         self.buffer.fill(text)
-        assert self.buffer.copyFromLine((0,0),0) == ""
+        assert self.buffer.copyFromLine((1,1),0) == ""
 
     def test_copyRange_inLine(self):
         text = "abc\ndef\nghi"
         self.buffer.fill(text)
         expect = "e"
-        assert self.buffer.copyRange((1,1), (1,1)) == expect
+        assert self.buffer.copyRange((2,2), (2,2)) == expect
         expect = "def"
-        assert self.buffer.copyRange((1,0), (1,2)) == expect
+        assert self.buffer.copyRange((2,1), (2,3)) == expect
 
     def test_copyRange_withoutBody(self):
         text = "abc\ndef\nghi\njkl"
         self.buffer.fill(text)
         expect = "ef\ngh"
-        assert self.buffer.copyRange((1,1), (2,1)) == expect
+        assert self.buffer.copyRange((2,2), (3,2)) == expect
 
     def test_copyRange_withBody(self):
         text = "abc\ndef\nghi\njkl"
         self.buffer.fill(text)
         expect = "ef\nghi\njk"
-        assert self.buffer.copyRange((1,1), (3,1)) == expect
+        assert self.buffer.copyRange((2,2), (4,2)) == expect
 
     def test_copyRange_onlyBody(self):
         text = "abc\ndef\nghi\njkl"
         self.buffer.fill(text)
         expect = "def\nghi"
-        assert self.buffer.copyRange((1,0), (2,2)) == expect
+        assert self.buffer.copyRange((2,1), (3,3)) == expect
 
     def test_copyRangeSingleChar(self):
         text = "abc\ndef\nghi\njkl"
         self.buffer.fill(text)
         expect = "e"
-        assert self.buffer.copyRange((1,1), (1,1)) == expect
+        assert self.buffer.copyRange((2,2), (2,2)) == expect
 
     """ deleting """
 
@@ -168,75 +168,75 @@ class TestBuffer:
         text = "1\n2\n3\n4"
         expect = "1\n4"
         self.buffer.fill(text)
-        self.buffer.deleteLines(1, 2)
+        self.buffer.deleteLines(2, 2)
         assert str(self.buffer) == expect
 
     def test_deleteAllLines(self):
         text = "1\n2\n3\n4"
         expect = ""
         self.buffer.fill(text)
-        self.buffer.deleteLines(0, 4)
+        self.buffer.deleteLines(1, 4)
         assert str(self.buffer) == expect
 
     def test_deleteNullLines(self):
         text = "1\n2\n3\n4"
         expect = text
         self.buffer.fill(text)
-        self.buffer.deleteLines(0, 0)
+        self.buffer.deleteLines(1, 0)
         assert str(self.buffer) == expect
 
     def test_deleteFromLine(self):
         text = "0\nabcde\n3"
         expect = "0\nade\n3"
         self.buffer.fill(text)
-        self.buffer.deleteFromLine((1,1),2)
+        self.buffer.deleteFromLine((2,2),2)
         assert str(self.buffer) == expect
 
     def test_deleteAllFromLine(self):
         text = "0\nabcde\n3"
         expect = "0\n\n3"
         self.buffer.fill(text)
-        self.buffer.deleteFromLine((1,0),5)
+        self.buffer.deleteFromLine((2,1),5)
         assert str(self.buffer) == expect
 
     def test_deleteNullFromLine(self):
         text = "0\nabcde\n3"
         expect = text
         self.buffer.fill(text)
-        self.buffer.deleteFromLine((0,0),0)
+        self.buffer.deleteFromLine((1,1),0)
         assert str(self.buffer) == expect
 
     def test_deleteRange_inLine(self):
         text = "abc\ndef\nghi"
         self.buffer.fill(text)
         expect = "abc\ndf\nghi"
-        self.buffer.deleteRange((1,1), (1,1))
+        self.buffer.deleteRange((2,2), (2,2))
         assert str(self.buffer) == expect
         text = "abc\ndef\nghi"
         self.buffer.fill(text)
         expect = "abc\n\nghi"
-        self.buffer.deleteRange((1,0), (1,2))
+        self.buffer.deleteRange((2,1), (2,3))
         assert str(self.buffer) == expect
 
     def test_deleteRange_withoutBody(self):
         text = "abc\ndef\nghi\njkl"
         self.buffer.fill(text)
         expect = "abc\ndi\njkl"
-        self.buffer.deleteRange((1,1), (2,1))
+        self.buffer.deleteRange((2,2), (3,2))
         assert str(self.buffer) == expect
 
     def test_deleteRange_withBody(self):
         text = "abc\ndef\nghi\njkl"
         self.buffer.fill(text)
         expect = "al"
-        self.buffer.deleteRange((0,1), (3,1))
+        self.buffer.deleteRange((1,2), (4,2))
         assert str(self.buffer) == expect
 
     def test_deleteRange_onlyBody(self):
         text = "abc\ndef\nghi\njkl"
         self.buffer.fill(text)
         expect = "abc\n\njkl"
-        self.buffer.deleteRange((1,0), (2,2))
+        self.buffer.deleteRange((2,1), (3,3))
         assert str(self.buffer) == expect
 
     """ inserting """
@@ -244,7 +244,7 @@ class TestBuffer:
     def test_insertLinesWhenEmpty(self):
         text = "1\n2\n3"
         expect = "1\n2\n3"
-        self.buffer.insertLines(0, text)
+        self.buffer.insertLines(1, text)
         assert str(self.buffer) == expect
 
     def test_insertLinesBetween(self):
@@ -252,7 +252,7 @@ class TestBuffer:
         text = "a\nb"
         expect = "1\na\nb\n2\n3"
         self.buffer.fill(prefill)
-        self.buffer.insertLines(1, text)
+        self.buffer.insertLines(2, text)
         assert str(self.buffer) == expect
 
     def test_insertLinesOnTop(self):
@@ -260,7 +260,7 @@ class TestBuffer:
         text = "a\nb"
         expect = "a\nb\n1\n2\n3"
         self.buffer.fill(prefill)
-        self.buffer.insertLines(0, text)
+        self.buffer.insertLines(1, text)
         assert str(self.buffer) == expect
 
     def test_insertLinesAtBottom(self):
@@ -268,7 +268,7 @@ class TestBuffer:
         text = "a\nb"
         expect = "1\n2\n3\na\nb"
         self.buffer.fill(prefill)
-        self.buffer.insertLines(3, text)
+        self.buffer.insertLines(4, text)
         assert str(self.buffer) == expect
 
     def test_insertLines_leadingNewline(self):
@@ -276,7 +276,7 @@ class TestBuffer:
         text = "\n5"
         expect = "1\n2\n3\n\n5"
         self.buffer.fill(prefill)
-        self.buffer.insertLines(3, text)
+        self.buffer.insertLines(4, text)
         assert str(self.buffer) == expect
 
     def test_insertLines_trailingNewline(self):
@@ -284,7 +284,7 @@ class TestBuffer:
         text = "4\n"
         expect = "1\n2\n3\n4\n"
         self.buffer.fill(prefill)
-        self.buffer.insertLines(3, text)
+        self.buffer.insertLines(4, text)
         assert str(self.buffer) == expect
 
     def test_insertLines_singleNewline(self):
@@ -292,7 +292,7 @@ class TestBuffer:
         text = "\n"
         expect = "1\n2\n3\n\n"
         self.buffer.fill(prefill)
-        self.buffer.insertLines(3, text)
+        self.buffer.insertLines(4, text)
         assert str(self.buffer) == expect
 
     def test_insert_empty_string(self):
@@ -300,7 +300,7 @@ class TestBuffer:
         text = ""
         expect = "1\n22\n3"
         self.buffer.fill(prefill)
-        self.buffer.insert((1,1), text)
+        self.buffer.insert((2,2), text)
         assert str(self.buffer) == expect
 
     def test_insert_simple_string(self):
@@ -308,7 +308,7 @@ class TestBuffer:
         text = "aa"
         expect = "1\n2aa2\n3"
         self.buffer.fill(prefill)
-        self.buffer.insert((1,1), text)
+        self.buffer.insert((2,2), text)
         assert str(self.buffer) == expect
 
     def test_insert_newline(self):
@@ -316,7 +316,7 @@ class TestBuffer:
         text = "\n"
         expect = "1\n2\n2\n3"
         self.buffer.fill(prefill)
-        self.buffer.insert((1,1), text)
+        self.buffer.insert((2,2), text)
         assert str(self.buffer) == expect
 
     def test_insert_surrounding_newlines(self):
@@ -324,7 +324,7 @@ class TestBuffer:
         text = "\naa\n"
         expect = "1\n2\naa\n2\n3"
         self.buffer.fill(prefill)
-        self.buffer.insert((1,1), text)
+        self.buffer.insert((2,2), text)
         assert str(self.buffer) == expect
 
     def test_insertWithoutBody(self):
@@ -332,7 +332,7 @@ class TestBuffer:
         text = "aa\nbb"
         expect = "1\n2aa\nbb2\n3"
         self.buffer.fill(prefill)
-        self.buffer.insert((1,1), text)
+        self.buffer.insert((2,2), text)
         assert str(self.buffer) == expect
 
     def test_insertWithBody(self):
@@ -340,6 +340,6 @@ class TestBuffer:
         text = "aa\nbb\ncc"
         expect = "1\n2aa\nbb\ncc2\n3"
         self.buffer.fill(prefill)
-        self.buffer.insert((1,1), text)
+        self.buffer.insert((2,2), text)
         assert str(self.buffer) == expect
 
