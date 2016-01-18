1c1875e035c9a56056d4ff15cb1bad090fd0a3fc
Elmar Hinz <t3elmar@gmail.com>
2016-01-18 13:26:04 +0100

    Makeing buffer based1

--- vii/Buffer.py
+++ vii/Buffer.py
@@ -7,6 +7,13 @@ class RangeExeption(IndexError): pass
 class Buffer:
     """
     All ranges are inclusive.
+
+    Indexing of all functions is based on 1
+    for y and x while the internal lines array
+    is based on 0 as usual in Python.
+
+    The y direction (linewise) is always named
+    before x.
     """
 
     updateSignal = "bufferUpdate"
@@ -17,12 +24,13 @@ class Buffer:
 
     def insert(self, position, string):
         "Insert string at position y, x"
+        "Plus1 for appending"
         if string == "": return
         y, x = position
         self._checkLineBoundsPlus1(y, x)
         tokens = self._parse(string)
-        head = self.lines[y][:x]
-        tail = self.lines[y][x:]
+        head = self.lines[y-1][:x-1]
+        tail = self.lines[y-1][x-1:]
         if len(tokens) == 1:
             body = tokens[0]
             replacement = [head + body + tail]
@@ -31,13 +39,14 @@ class Buffer:
             body = tokens[1:-1]
             tail = tokens[-1] + tail
             replacement = [head] + body + [tail]
-        self.lines[y:y+1] = replacement
+        self.lines[y-1:y] = replacement
         signal(self.updateSignal, self)
 
     def insertLines(self, y, text):
         "Insert text at y as full lines"
+        "Plus1 for appending"
         self._checkBufferBoundsPlus1(y)
-        self.lines[y:y] = self._parse(text)
+        self.lines[y-1:y-1] = self._parse(text)
         signal(self.updateSignal, self)
 
     def deleteFromLine(self, position, count):
@@ -46,15 +55,15 @@ class Buffer:
         y, x = position
         self._checkLineBounds(y, x)
         self._checkLineBounds(y, x + count - 1)
-        line = self.lines[y]
-        self.lines[y] = line[:x] + line[x+count:]
+        line = self.lines[y-1]
+        self.lines[y-1] = line[:x-1] + line[x-1+count:]
         signal(self.updateSignal, self)
 
     def deleteLines(self, y, count):
         if count == 0: return
         self._checkBufferBounds(y)
         self._checkBufferBounds(y+count-1)
-        self.lines[y:y+count] = []
+        self.lines[y-1:y-1+count] = []
         "Delete count lines from y"
         signal(self.updateSignal, self)
 
@@ -62,9 +71,9 @@ class Buffer:
         "Delete from position to position"
         start, end = self._checkRange(start, end)
         y1, x1 = start; y2, x2 = end
-        head = self.lines[y1][:x1]
-        tail = self.lines[y2][x2+1:]
-        self.lines[y1:y2+1] = [head + tail]
+        head = self.lines[y1-1][:x1-1]
+        tail = self.lines[y2-1][x2-1+1:]
+        self.lines[y1-1:y2] = [head + tail]
         signal(self.updateSignal, self)
 
     def copyFromLine(self, position, count):
@@ -73,14 +82,14 @@ class Buffer:
         y, x = position
         self._checkLineBounds(y, x)
         self._checkLineBounds(y, x + count - 1)
-        return self.lines[y][x:x+count]
+        return self.lines[y-1][x-1:x-1+count]
 
     def copyLines(self, y, count):
         "Copy count lines from y"
         if count == 0: return ""
         self._checkBufferBounds(y)
         self._checkBufferBounds(y + count - 1)
-        return self._join(self.lines[y:y+count])
+        return self._join(self.lines[y-1:y-1+count])
 
     def copyRange(self, start, end):
         "Copy from position to position"
@@ -90,9 +99,9 @@ class Buffer:
             count = x2 - x1 + 1
             return self.copyFromLine(start, count)
         else:
-            count = self.lengthOfLine(y1) - x1
+            count = self.lengthOfLine(y1) - x1 + 1
             head = self.copyFromLine(start, count)
-            tail = self.copyFromLine((y2, 0), x2 + 1)
+            tail = self.copyFromLine((y2, 1), x2)
             if y2 - y1 >= 2:
                 body = self.copyLines(y1 + 1, y2 - y1 - 1)
                 return '\n'.join([head, body, tail])
@@ -101,6 +110,7 @@ class Buffer:
 
     def lengthOfLine(self, y):
         "Length of line y"
+        y = y - 1
         self._checkBufferBounds(y)
         return len(self.lines[y])
 
@@ -110,8 +120,8 @@ class Buffer:
 
     def fill(self, text):
         "Clear and fill the buffer with text"
-        self.deleteLines(0, self.countOfLines())
-        self.insertLines(0, text)
+        self.deleteLines(1, self.countOfLines())
+        self.insertLines(1, text)
 
     def _parse(self, string):
         "Parse to list of lines"
@@ -128,52 +138,35 @@ class Buffer:
         return self._join(self.lines)
 
     def _checkBufferBounds(self, y):
-        if y < 0:
-            raise BufferBoundsException()
-        if y >= len(self.lines):
-            raise BufferBoundsException()
-
-    def _checkBufferBoundsPlus1(self, y):
-        if y < 0:
+        if y < 1:
             raise BufferBoundsException()
         if y > len(self.lines):
             raise BufferBoundsException()
 
+    def _checkBufferBoundsPlus1(self, y):
+        if y < 1:
+            raise BufferBoundsException()
+        if y > len(self.lines) + 1:
+            raise BufferBoundsException()
+
     def _checkLineBounds(self, y, x):
         self._checkBufferBounds(y)
-        if x < 0:
+        if x < 1:
             raise LineBoundsException("1: y/x: %s/%s"%(y,x))
-        line = self.lines[y]
-        if x >= len(line):
+        line = self.lines[y-1]
+        if x > len(line):
             raise LineBoundsException("2: y/x: %s/%s"%(y,x))
 
     def _checkLineBoundsPlus1(self, y, x):
         self._checkBufferBounds(y)
         if x < 0:
             raise LineBoundsException("3: y/x: %s/%s"%(y,x))
-        line = self.lines[y]
+        line = self.lines[y-1]
         if x > len(line):
             raise LineBoundsException("4: y/x: %s/%s"%(y,x))
 
     def _checkRange(self, start, end):
         y1, x1 = start; y2, x2 = end
-
-        if False: # Range Adjustments may be optional in future
-
-            if y1 < 0: y1 = 0
-            if x1 < 0: x1 = 0
-            if y2 < 0: y2 = 0
-            if x2 < 0: x2 = 0
-
-            yLimit = len(self.lines) - 1
-            if y1 > yLimit: y1 = yLimit
-            if y2 > yLimit: y2 = yLimit
-
-            xLimit1 = len(self.lines[y1]) - 1
-            if x1 > xLimit1: x1 = xLimit1
-
-            xLimit2 = len(self.lines[y2]) - 1
-            if x2 > xLimit2: x2 = xLimit2
 
         if y1 > y2: raise RangeExeption()
         if y1 == y2 and x1 > x2: raise RangeExeption()
