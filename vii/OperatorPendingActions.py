39ca6a95496d6c350304365cded615d633a1965e
Elmar Hinz <t3elmar@gmail.com>
2016-01-20 12:51:58 +0100

    Updating Rang and wire it

--- vii/OperatorPendingActions.py
+++ vii/OperatorPendingActions.py
@@ -1,4 +1,5 @@
 from .AbstractAction import AbstractAction
+from .Range import Range
 
 class Left(AbstractAction):
     def act(self, callback):
@@ -6,14 +7,14 @@ class Left(AbstractAction):
         stop = self.ranges.left()
         start = self.ranges.left(factor)
         self.cursor.left(factor)
-        return callback.call((start, stop))
+        return callback.call(Range(start, stop))
 
 class Right(AbstractAction):
     def act(self, callback):
         factor = self.command.multiplyAll()
         start = self.cursor.position()
         stop = self.ranges.right(factor - 1)
-        return callback.call((start, stop))
+        return callback.call(Range(start, stop))
 
 class YankLines(AbstractAction):
     def act(self, callback):
