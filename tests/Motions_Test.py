b46d26e4b8bf9f1fdbe653df632652e083506463
Elmar Hinz <t3elmar@gmail.com>
2016-01-25 14:12:34 +0100

    Motions, adjusting to Range/Position

--- tests/Motions_Test.py
+++ tests/Motions_Test.py
@@ -1,6 +1,7 @@
 # from nose.tools import *
 from nose.plugins.skip import SkipTest
 from vii.Motions import Motions
+from vii.Range import Range, Position
 
 class Dummy():
     text = ["aa", "bbb", "ccccc", "ddd", "ee"]
@@ -10,6 +11,9 @@ class Dummy():
 
     def countOfLines(self):
         return len(self.text)
+
+    def position(self):
+        return Position(self.y, self.x)
 
 class Motions_Test:
     def setup(self):
@@ -21,36 +25,40 @@ class Motions_Test:
         self.cursor.x = 3
         self.cursor.y = 3
 
+    def range(self, y, x):
+        return Range(self.cursor.position().toPosition(),
+                (y, x))
+
     def testUp(self):
-        assert self.fixture.up() == (2,3)
-        assert self.fixture.up(2) == (1,2)
-        assert self.fixture.up(3) == (1,2)
+        assert self.fixture.up() == self.range(2,3)
+        assert self.fixture.up(2) == self.range(1,2)
+        assert self.fixture.up(3) == self.range(1,2)
 
     def testDown(self):
-        assert self.fixture.down() == (4,3)
-        assert self.fixture.down(2) == (5,2)
-        assert self.fixture.down(3) == (5,2)
+        assert self.fixture.down() == self.range(4,3)
+        assert self.fixture.down(2) == self.range(5,2)
+        assert self.fixture.down(3) == self.range(5,2)
 
     def testLeft(self):
-        assert self.fixture.left() == (3,2)
-        assert self.fixture.left(2) == (3,1)
-        assert self.fixture.left(3) == (3,1)
+        assert self.fixture.left() == self.range(3,2)
+        assert self.fixture.left(2) == self.range(3,1)
+        assert self.fixture.left(3) == self.range(3,1)
 
     def testRight(self):
-        assert self.fixture.right() == (3,4)
-        assert self.fixture.right(2) == (3,5)
-        assert self.fixture.right(3) == (3,5)
+        assert self.fixture.right() == self.range(3,4)
+        assert self.fixture.right(2) == self.range(3,5)
+        assert self.fixture.right(3) == self.range(3,5)
 
     def testBeginningOfBuffer(self):
-        assert self.fixture.beginningOfBuffer() == (1, 1)
+        assert self.fixture.beginningOfBuffer() == self.range(1, 1)
 
     def testBeginningOfLine(self):
-        assert self.fixture.beginningOfLine() == (3,1)
+        assert self.fixture.beginningOfLine() == self.range(3,1)
 
     def testEndOfBuffer(self):
-        assert self.fixture.endOfBuffer() == (5,2)
+        assert self.fixture.endOfBuffer() == self.range(5,2)
 
     def testEndOfLine(self):
-        assert self.fixture.endOfLine() == (3,5)
-        assert self.fixture.endOfLine(2) == (4,3)
+        assert self.fixture.endOfLine() == self.range(3,5)
+        assert self.fixture.endOfLine(2) == self.range(4,3)
 
--- vii/Motions.py
+++ vii/Motions.py
@@ -1,3 +1,5 @@
+from .Range import Range
+from .Logger import debug
 
 class Motions:
     """
@@ -10,44 +12,49 @@ class Motions:
         self.cursor = None
 
     def appendInLine(self):
-        range = (self._y(), self._x() + 1)
-        return self._forceLimits(range, 1)
+        position = (self._y(), self._x() + 1)
+        return (self._toRange(
+            self._forceLimits(position, 1)))
 
     def beginningOfBuffer(self):
-        return (1, 1)
+        return self._toRange((1, 1))
 
     def beginningOfLine(self):
-        return (self._y(), 1)
+        return self._toRange((self._y(), 1))
 
     def down(self, step = None):
         if not step: step = 1
-        range = self._y() + step, self._x()
-        return self._forceLimits(range)
+        position = self._y() + step, self._x()
+        return (self._toRange(
+            self._forceLimits(position)))
 
     def endOfBuffer(self):
         y = self.buffer.countOfLines()
         x = self.buffer.lengthOfLine(y)
-        return (y, x)
+        return self._toRange((y, x))
 
     def endOfLine(self, step = None):
         if not step: step = 1
         y = self._y() + step - 1
-        return (y, self.buffer.lengthOfLine(y) )
+        return self._toRange((y, self.buffer.lengthOfLine(y) ))
 
     def left(self, step = None):
         if not step: step = 1
-        range = (self._y(), self._x() - step)
-        return self._forceLimits(range)
+        position = (self._y(), self._x() - step)
+        return self._toRange(
+                self._forceLimits(position))
 
     def right(self, step = None):
         if not step: step = 1
-        range = (self._y(), self._x() + step)
-        return self._forceLimits(range)
+        position = (self._y(), self._x() + step)
+        return self._toRange(
+                self._forceLimits(position))
 
     def up(self, step = None):
         if not step: step = 1
-        range = (self._y() - step, self._x())
-        return self._forceLimits(range)
+        position = (self._y() - step, self._x())
+        return self._toRange(
+                self._forceLimits(position))
 
     def _x(self):
         return self.cursor.x
@@ -64,4 +71,7 @@ class Motions:
         if x > xLimit: x = xLimit
         if x < 1: x = 1
         return (y, x)
+
+    def _toRange(self, position):
+        return Range(self.cursor.position().toPosition(), position)
 
