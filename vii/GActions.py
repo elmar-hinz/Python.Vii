d1c7b6d578798fd01010a1577170cd91cff932cd
Elmar Hinz <t3elmar@gmail.com>
2016-01-28 11:46:04 +0100

    J and gJ actions

--- vii/GActions.py
+++ vii/GActions.py
@@ -2,7 +2,6 @@ from .AbstractAction import AbstractAction
 from .Range import Range, Position
 from .Logger import debug
 
-
 class GotoTop(AbstractAction):
     def act(self, callback = None):
         y = self.command.multiplyAll()
@@ -13,4 +12,23 @@ class GotoTop(AbstractAction):
             self.cursor.move(motion)
             self.finish()
             return "normal", self.actionManager.action("normal", "idle")
+
+class JoinLinesWithoutAdjustments(AbstractAction):
+    def act(self):
+        factor = self.command.multiplyAll()
+        y = self.cursor.y
+        for i in range(factor):
+            if y < self.buffer.countOfLines():
+                joinPosition = Position(y, self.buffer.lengthOfLine(y))
+                # firstLine without newline
+                firstLine = self.buffer.copy(Range(y,y))
+                firstLine = firstLine[:-1]
+                firstLengthTrimmed = len(firstLine.rstrip())
+                lastLine = self.buffer.copy(Range(y+1, y+1))
+                self.buffer.delete(Range(y, y+1))
+                joined = firstLine + lastLine
+                self.buffer.insert(Position(y, 1), joined)
+                self.cursor.gotoPositionStrict(joinPosition)
+        return "normal", self.actionManager.action("normal", "idle")
+
 
